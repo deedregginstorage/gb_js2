a311f29560fc713b71323ef8b114a2fe204da19d
Anatoly <anatoliy.kozhevnikov@gmail.com>
2021-03-05 19:02:28 +0300

    lesson1 added

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
+const goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 50
+    },
+    {
+        title: 'Jacket',
+        price: 350
+    },
+    {
+        title: 'Shoes',
+        price: 250
+    },
 ];
 
 
 const $goodsList = document.querySelector('.goods-list');
-  
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+
+const renderGoodsItem = ({
+    title,
+    price
+}) => {
+    return `<div class="goods-item"><img src="#" width="250" height="180"><h3>${title}</h3><p>${price}</p><button class="button">Добавить в корзину</button></div>`;
 };
-  
+
 const renderGoodsList = (list = goods) => {
     let goodsList = list.map(
-            item => renderGoodsItem(item)
-        );
+        item => renderGoodsItem(item)
+    ).join('\n');
 
     $goodsList.insertAdjacentHTML('beforeend', goodsList);
 }
-  
+
 renderGoodsList();
